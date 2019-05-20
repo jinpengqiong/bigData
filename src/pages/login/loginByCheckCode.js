625e094c260edd4d074c83c265c8eb37d53ca1db
King <jinpengguan@126.com>
2019-05-20 18:20:24 +0800

    add logins

--- src/pages/login/loginByCheckCode.js
+++ src/pages/login/loginByCheckCode.js
@@ -1,13 +1,16 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Button, Text, Image } from '@tarojs/components'
-import { AtForm, AtInput, AtButton } from 'taro-ui'
+import { AtForm, AtInput, AtButton, AtMessage } from 'taro-ui'
 
 import './loginByCheckCode.less'
+import { clearInterval } from 'timers';
 
 export default class LoginByCheckCode extends Component {
   state = {
     phone : '',
     checkCode:'',
+    countDown:59,
+    isDisabled:false
   }
 
   componentWillMount () { }
@@ -34,6 +37,35 @@ export default class LoginByCheckCode extends Component {
 
   }
 
+  sendCheckCode = () => {
+    const { countDown, phone } = this.state
+    if(!phone){
+      Taro.atMessage({
+        'message': '请先输入手机号',
+        'type': 'error',
+      })
+      return
+    }
+    this.setState({
+      isDisabled:true
+    })
+    let count = 59
+    let timer = setInterval(() => {
+      count = count-1
+      this.setState({
+        countDown:count
+      })
+      if(countDown<=0){
+        clearInterval(timer)
+        this.setState({
+          countDown:59,
+          isDisabled:false
+        })
+      }
+    }, 1000)
+
+  }
+
   render () {
     return (
       <View>
@@ -55,12 +87,24 @@ export default class LoginByCheckCode extends Component {
             value={this.state.checkCode}
             onChange={this.handleInputChange}
           >
-            <AtButton type='primary' size='small' className="checkCode">验证码</AtButton>
+            <AtButton
+            type='primary'
+            size='small'
+            className="checkCode"
+            onClick={this.sendCheckCode}
+            disabled={this.state.isDisabled}
+            >
+              {
+                this.state.isDisabled?
+                this.state.countDown
+                :
+                '验证码'
+              }
+            </AtButton>
           </AtInput>
         </AtForm>
         <AtButton type='primary' size='normal' circle={true} className='confirm' onClick={this.redirect}>确定</AtButton>
       </View>
-
     )
   }
 }
